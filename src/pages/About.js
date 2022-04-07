f85a26e0efb829f9de177fe03db328a9647a999e
John David Delgado <johndavid@Johns-MacBook-Pro.local>
2022-04-06 23:40:24 -0400

    about me update

--- src/pages/About.js
+++ src/pages/About.js
@@ -16,64 +16,40 @@ export default function About() {
           color: '#7A7A7A',
         }}
       >
-        <Box sx={{ textAlign: 'center', marginRight: '80px', display: {
-          mobile: 'none',
-          tablet: 'block',
-          laptop: 'block',
-          desktop: 'block',
-        } }}>
+        <Box
+          sx={{
+            textAlign: 'center',
+            marginRight: '80px',
+            display: {
+              mobile: 'none',
+              tablet: 'block',
+              laptop: 'block',
+              desktop: 'block',
+            },
+          }}
+        >
           <img className="profile-pic" src="./images/profile.jpg" alt="" />
           <Typography variant="h5" color="white">
             Contact Details
           </Typography>
           <Typography variant="p">johndavid.p.delgado@gmail.com</Typography>
         </Box>
-        <Box sx={{ 
-          width: {
-            mobile: '100%',
-            tablet: '80%',
-            laptop: '60%',
-            desktop: '50%'
-          }, 
-          lineHeight: '30px' }}>
+        <Box
+          sx={{
+            width: {
+              mobile: '100%',
+              tablet: '80%',
+              laptop: '60%',
+              desktop: '50%',
+            },
+            lineHeight: '30px',
+          }}
+        >
           <Typography variant="h5" color="white">
             About Me
           </Typography>
           <Typography variant="p">
-            "Seize the day and make your lives extra ordinary". A guideline I
-            live by through every waking moment to start my day. It is my staple
-            point on how my day is going to be and how my week is going to turn
-            out.
-            <br />
-            <br />
-            I am a 10 year Army Veteran with extensive experience in the
-            Information Systems and Information Security working on Network
-            Systems, Network Operations, Communication Security and Security
-            Compliance. I'm currently pursuing a career in Software Development
-            to apply my creative and critical thinking skills to solve complex
-            problems to develop software applications to improve organization
-            efficiency.
-            <br />
-            <br />
-            During my tenure in the Army I've learned several things about
-            myself, but the one thing that keeps me driving forward is my
-            perseverance to succeed especially during the low points and the
-            failure points in my life. As an aspiring Software Developer, my
-            skillset fell right into place and felt at home immediately with the
-            complexity of Software Development and the process it takes to{' '}
-            <strong>
-              <em>CREATE.</em>
-            </strong>
-            <br />
-            <br />
-            Despite the hard learning and the struggles of trouble shooting, my
-            experience in the Army has helped me to be composed and to be
-            confident that I will utilize my Critical, and Creative Thinking
-            abilities to find the best solution to a problem. With my experience
-            in software development, I find the greatest reward is the smile and
-            joy behind the screen, watching my project work the way I intended
-            to. The struggles and frustrations that came with it, earns a share
-            with that reward.
+            I am a 10 year Army Veteran with a passion for creating and problem solving. I will always be a nerd at heart and will never be bored with technology. Learning something new every day is my motivation to create and solve something new everyday. My new found passion for Software Development has awaken the child in me to keep exploring and finding joy in the little things that new challenges has to offer.
           </Typography>
         </Box>
       </Box>
